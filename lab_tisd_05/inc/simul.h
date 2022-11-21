2e5aa2be28e8ad2aafdeea0892fd74947600c920
gga21u142 <114350386+gga21u142@users.noreply.github.com>
2022-11-21 23:27:35 +0300

    Delete lab_tisd_05 directory

--- lab_tisd_05/inc/simul.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SIMUL_H__
-#define __SIMUL_H__
-
-#include "queue.h"
-
-int simulate_arr(double t1_a, double t1_b, double t2_a, double t2_b);
-int simulate_list(double t1_a, double t1_b, double t2_a, double t2_b);
-
-#endif
--- lab_tisd_05/inc/test.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TEST_H__
-#define __TEST_H__
-
-void test_call(FILE *fres, FILE *frep,
-	double t1_a, double t1_b, double t2_a, double t2_b);
-void test();
-
-#endif
